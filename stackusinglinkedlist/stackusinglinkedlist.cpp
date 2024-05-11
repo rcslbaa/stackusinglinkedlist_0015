755a64eedfd9cd6829b03ec91d88015a7e6badc2
rcslbaa <ifasmarani3125@gmail.com>
2024-05-11 10:39:31 +0700

    17 menambahkan while, memberi output dan input

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -69,3 +69,11 @@ int main() {
 
 	int choice = 0;
 	int value;
+
+	while (choice != 5) {
+		cout << "1. Push\n";
+		cout << "2. Pop\n";
+		cout << "3. Peek\n";
+		cout << "4. Exit\n";
+		cout << "Enter yout choice: ";
+		cin >> choice;
