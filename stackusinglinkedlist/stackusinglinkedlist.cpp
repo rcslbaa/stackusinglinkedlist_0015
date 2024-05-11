c0db1a49ffd2c72b3a04aa3ea63297da750b4156
rcslbaa <ifasmarani3125@gmail.com>
2024-05-11 10:27:47 +0700

    06 menetapkan nilai data

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -25,4 +25,5 @@ public:
 	// Push operation: Insert an element onto the top of the stack
 	int push(int value) {
 		Node* newNode = new Node(); // 1. Allocate memory for the new node
+		newNode->data = value; // 2. Assign value
 		
