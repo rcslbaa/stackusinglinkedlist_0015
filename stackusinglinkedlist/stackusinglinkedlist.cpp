2ca0719f0ceb6618b30aeff2b99eb9060a83d167
rcslbaa <ifasmarani3125@gmail.com>
2024-05-11 10:33:56 +0700

    10 menambahkan void pop

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -31,3 +31,10 @@ public:
 		cout << "Push value: " << value << endl;
 		return value;
 	}
+
+	// Pop operation: Remove the topmost element from the stack
+	void pop() {
+		if (isEmpty()) {
+			cout << "Stack is empty." << endl;
+		}
+		
